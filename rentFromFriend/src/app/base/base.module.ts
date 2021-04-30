be37fe63a2ac13ceb26389d291d726f562269897
Hoseen Eshtay <eshtay@doehle.de>
2021-04-30 13:24:55 +0200

    deleted routing/ deleted html template

--- rentFromFriend/src/app/base/base.module.ts
+++ rentFromFriend/src/app/base/base.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BaseRoutingModule } from './base-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BaseRoutingModule, SharedModule],
+  imports: [CommonModule, SharedModule],
 })
 export class BaseModule {}
