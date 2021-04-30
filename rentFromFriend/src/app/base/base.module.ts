ff7fa331897c652f883cc5c253cb7e117e015a20
Hoseen Eshtay <eshtay@doehle.de>
2021-04-30 12:13:40 +0200

    created base module

---
+++ rentFromFriend/src/app/base/base.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseRoutingModule } from './base-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BaseRoutingModule
+  ]
+})
+export class BaseModule { }
