b80edc3884aadad1ebe1b58152b0c6f2935c20b0
Mathis Gertz <mathis.gertz@sinnerschrader.com>
2021-04-30 12:30:55 +0200

    added shared modules

---
+++ rentFromFriend/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// verschiedenen Module die wir benutzen wollen, importerien wir hier herein, um sie danach überall verfügbar zu haben
+// Besonders für Module die wir überall brauchen
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [],
+})
+export class SharedModule {}
