9229fc4e195fa4b3cbfbb9606ad297fc0940b14d
Mathis Gertz <mathis.gertz@sinnerschrader.com>
2021-04-30 12:29:43 +0200

    added shared module

--- rentFromFriend/src/app/base/base.module.ts
+++ rentFromFriend/src/app/base/base.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseRoutingModule } from './base-routing.module';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    BaseRoutingModule
-  ]
+  imports: [CommonModule, BaseRoutingModule, SharedModule],
 })
-export class BaseModule { }
+export class BaseModule {}
