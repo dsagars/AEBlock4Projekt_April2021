073adb908e898412592ff2ef2df2e0099fbb4805
Emal M <emal.mashal@outlook.com>
2021-05-02 23:17:37 +0200

    -create login components inside login module

--- rentFromFriend/src/app/app.module.ts
+++ rentFromFriend/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    SharedModule, 
+    // Added forms module for login page
+    FormsModule, 
+    // Added Firebase Module 
+    AngularFireModule.initializeApp(environment.firebase) 
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- rentFromFriend/src/app/login/login.module.ts
+++ rentFromFriend/src/app/login/login.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
-
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    LoginRoutingModule,  
+    LoginRoutingModule,
   ]
 })
 export class LoginModule { }
