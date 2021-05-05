b775f264118956104ac82bcd7b69df873472ce79
Hoseen Eshtay <eshtay@doehle.de>
2021-05-05 18:46:39 +0200

    added user account button

--- rentFromFriend/src/app/shared/components/navbar/navbar.component.ts
+++ rentFromFriend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ModalService } from '../../services/modal.service';
+import { SidenavService } from '../../services/sidenav.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,34 @@ import { ModalService } from '../../services/modal.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(public modalService: ModalService) {}
+  constructor(
+    public modalService: ModalService,
+    private router: Router,
+    private sidenavService: SidenavService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  openSidenav() {
+    this.sidenavService.open();
+  }
+
+  navigateToProfile() {
+    this.router.navigate(['account/profile']);
+    this.openSidenav();
+  }
+
+  navigateToMessage() {
+    this.router.navigate(['account/message']);
+    this.openSidenav();
+  }
+
+  navigateToManageFriend() {
+    this.router.navigate(['account/manage-friend']);
+    this.openSidenav();
+  }
+
+  navigateToSetting() {
+    this.router.navigate(['account/setting']);
+    this.openSidenav();
+  }
 }
