1c87a4318e685090bbea3262dead5c57a5009a47
Emal M <emal.mashal@outlook.com>
2021-05-07 15:02:20 +0200

    outsourced login

--- rentFromFriend/src/app/app-routing.module.ts
+++ rentFromFriend/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'account',
     loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'base',
+    component: BaseComponent
+  },
 ];
 
 @NgModule({
