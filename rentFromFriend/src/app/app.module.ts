3b6c58b47c17d4c7f3c9542de1d8dad6021ffafc
Emal M <emal.mashal@outlook.com>
2021-05-07 11:03:25 +0200

    removed home folder

--- rentFromFriend/src/app/app.module.ts
+++ rentFromFriend/src/app/app.module.ts
@@ -6,12 +6,11 @@ import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule, 
     AppRoutingModule, 
--- rentFromFriend/src/app/login/login.component.ts
+++ rentFromFriend/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
-import { FirebaseService } from 'src/app/services/firebase.service';
+import { FirebaseService } from 'src/app/shared/services/firebase.service';
 
 @Component({
   selector: 'app-login',
