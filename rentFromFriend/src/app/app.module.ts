c3a163a6dd2af2708c8b4b61613709006dcd319d
Emal M <emal.mashal@outlook.com>
2021-05-07 10:50:50 +0200

    many changes before merge

--- rentFromFriend/src/app/app.module.ts
+++ rentFromFriend/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule, 
     AppRoutingModule, 
---
+++ rentFromFriend/src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { FirebaseService } from 'src/app/services/firebase.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  isSignedIn = false;
+  constructor(public firebaseService : FirebaseService){}
+  ngOnInit(){
+    if(localStorage.getItem('user') !== null){
+      // set true if you want to save in browser. The user will be logged in automatically
+      // (if you do not see the signup/login form, set to false!)
+      this.isSignedIn = false; 
+    } else {
+      this.isSignedIn = false;
+    }
+  }
+
+  async onSignup(email: string, password: string){
+    await this.firebaseService.signup(email, password);
+    if(this.firebaseService.isLoggedIn){
+      this.isSignedIn = true;
+    }
+  }
+
+  async onSignin(email: string, password: string){
+    await this.firebaseService.signin(email, password);
+    if(this.firebaseService.isLoggedIn){
+      this.isSignedIn = true;
+    }
+  }
+
+  handleLogout(){
+    this.isSignedIn = false;
+  }
+
+  // new testing for crud
+  async add(){
+    await this.firebaseService.add();
+  }
+  
+}
