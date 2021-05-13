44f5a5b9f7f00a5bfc49c71159d7ce4df855d1ee
Hoseen Eshtay <eshtay@doehle.de>
2021-05-14 01:15:55 +0200

    created login-guard

---
+++ rentFromFriend/src/app/shared/guards/login.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FirebaseService } from '../services/firebase.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(private router: Router, private firebaseService: FirebaseService) { }
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.firebaseService.isLoggedIn()) {
+      console.log(this.firebaseService.isLoggedIn());
+      this.router.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+}
