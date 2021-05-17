af6485abaf4203441341c234ec6e648ab36d73f1
Hoseen Eshtay <eshtay@doehle.de>
2021-05-17 23:29:03 +0200

    updated account component

--- rentFromFriend/src/app/account/account.component.ts
+++ rentFromFriend/src/app/account/account.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { SidenavService } from '../shared/services/sidenav.service';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
   showFiller = false;
-  @ViewChild('drawer') public sidenav: MatSidenav;
-  constructor(public sidenavService: SidenavService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-  }
-  
-  ngAfterViewInit(): void {
-    this.sidenavService.setSidenav(this.sidenav);
-    this.sidenavService.open();
+
   }
 
 }
