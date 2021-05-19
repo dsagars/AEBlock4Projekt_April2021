c957510f32984a78b6493b4be3f9c2569cc62cc5
sagar dahal <dahal@hanseaticsoft.com>
2021-05-20 00:44:25 +0200

    Add CategoryEnum class

---
+++ rentFromFriend/src/app/shared/models/category.enum.ts
@@ -0,0 +1,8 @@
+//These enums are use to select the item category and render the carousel accordingly
+export enum ItemCategoryEnum {
+    outdoor="Outdoor",
+    household="Haushalt",
+    electro="Elektro",
+    tools="Werkzeug",
+    games="Spiele",
+}
