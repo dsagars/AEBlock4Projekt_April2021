90b62a2a35d80a83bbd7e2be7efb1476f3a668e8
Emal M <emal.mashal@outlook.com>
2021-05-19 14:10:45 +0200

    Erweiterung des Notifier Services  um den User nach dem Ok zum Login page zu schicken

--- rentFromFriend/src/app/shared/services/notifier.service.ts
+++ rentFromFriend/src/app/shared/services/notifier.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,7 @@ import { DOCUMENT } from '@angular/common';
 export class NotifierService {
   constructor(
     private snackBar: MatSnackBar,
+    private router: Router,
     @Inject(DOCUMENT) private _document: Document
   ) {}
 
@@ -17,6 +19,16 @@ export class NotifierService {
     this.snackBar.open(displayMessage, displayButtonText, {
       horizontalPosition: 'center',
       verticalPosition: 'top',
+    });
+  }
+
+  showBasicAndNavigateToLogin(displayMessage: string, displayButtonText: string) {
+    let snackBar = this.snackBar.open(displayMessage, displayButtonText, {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+    snackBar.afterDismissed().subscribe(() => {
+      this.router.navigate(['/login']);
     });
   }
 
